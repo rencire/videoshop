d27c04fb21acc966f654189bb7d705e9f865928f
Eric Ren <eric.ren@macys.com>
2015-09-16 14:14:33 -0700

    using multer to upload file

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var fs = require('fs');
+var multer = require('multer');
+
+var done = false;
+var upload = multer({
+  dest:'./uservids/',
+  rename: function (fieldname, filename) {
+    return filename+Date.now();
+  },
+  onFileUploadStart: function (file) {
+    console.log(file.originalname + ' is starting ...')
+  },
+  onFileUploadComplete: function (file) {
+    console.log(file.fieldname + ' uploaded to  ' + file.path)
+    done=true;
+  }
+});
+
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
+
+app.set('port', (process.env.PORT || 5000));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -19,8 +37,30 @@ app.get('/', function(request, response) {
     })
 });
 
+var count = 0;
+app.post('/api/upload', upload.single('video'), function(req, res) {
+  var filename = 'test' + count.toString();
+    // debugger
+    console.log(req.body);
+    console.log(req.file);
+     if(done==true){
+       console.log(req.files);
+       res.end("File uploaded.");
+     }
+    // console.log(req.files.video);
+    // fs.writeFile("./uservids/" + filename, req.body, function(err) {
+    //     if(err) {
+    //       return console.log(err);
+    //     }
+    //     console.log("The file " + filename + "was saved!");
+    //     count +=1;
+    // }); 
+});
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
 
 
