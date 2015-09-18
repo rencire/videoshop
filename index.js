231ece19f33c0ddcea2d01ea32e6b8388bc8f8af
Eric Ren <eric.ren@macys.com>
2015-09-17 20:26:56 -0700

    small changes to index.js

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ app.get('/', function(request, response) {
 
 app.get('/upload', function(req, res) {
   res.render('pages/upload', {
+     
   });
 });
 
@@ -134,12 +135,8 @@ app.get('/api/sign_s3', function(req, res) {
 });
 
 app.post('/upload', function(req, res) {
-     // if(done==true){
-     //   console.log(req.files);
-     //   res.end("File uploaded.");
-     // }
-     console.log(req.body);
-     res.end("Video uploaded!");
+   console.log(req.body);
+   res.redirect('/');
 });
 
 app.get('/api/videofiles', function(req, res) {
